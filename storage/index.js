c8ea356825ad2c7ccf9ee60fee5a5ffd5d0c5783
ArvinHsieh <arvinhsieh@outlook.com>
2024-12-29 20:40:21 +0800

    調整monitor畫面RWD

--- storage/index.js
+++ storage/index.js
@@ -1,6 +1,7 @@
 import config from '../config/index.js';
 import { createEnvironment, ENV_TYPE_PLAIN, updateEnvironment } from '../services/vercel.js';
 import { fetchEnvironment } from '../utils/index.js';
+import { createClient } from 'redis';
 
 const ENV_KEY = 'APP_STORAGE';
 
@@ -8,6 +9,8 @@ class Storage {
   env;
 
   data = {};
+
+  redisClient;
 
   async initialize() {
     if (!config.VERCEL_ACCESS_TOKEN) return;
@@ -21,6 +24,16 @@ class Storage {
       this.env = data.created;
     }
     this.data = JSON.parse(this.env.value);
+  }
+
+  async initializeRedis() {
+    // 創建 Redis 客戶端
+    this.redisClient = createClient();
+    // 監控連線錯誤
+    this.redisClient.on('error', (err) => console.error('Redis Client Error', err));
+
+    await this.redisClient.connect(); // 連接 Redis 伺服器
+    console.log('Connected to Redis');
   }
 
   /**
@@ -44,6 +57,27 @@ class Storage {
       type: ENV_TYPE_PLAIN,
     });
   }
+
+  async getCache(key) {
+    try {
+      // 讀取資料 (Read)
+      const userData = await client.get(key);
+      console.log('Data Read:', JSON.parse(userData)); // { id: 1, name: 'Alice', age: 25 }
+    } catch (err) {
+      console.error('Redis Client Error:', err);
+    }
+  }
+
+  async setCache(key, objData) {
+    try {
+      // 1. 建立資料 (Create)
+      await client.set(key, JSON.stringify(objData));
+      console.log('Data Created: ....');
+    } catch (err) {
+      console.error('Redis Client Error:', err);
+    }
+  }
+
 }
 
 const storage = new Storage();
